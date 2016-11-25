1833aae8867c1393b119934606bb942353e43ed3
asxcoder <xcodersun@gmail.com>
2016-11-25 10:46:45 -0800

    Added Menu of Home
    - carousol for welcome
    - three panels for channels, devices and dashboards

---
+++ src/js/controllers/menu-home.js
@@ -0,0 +1,21 @@
+angular.module('RDash')
+    .controller('MenuHome', ['$scope', MenuHome]);
+
+function MenuHome($scope) {
+  $scope.myInterval = 3000;
+  $scope.noWrapSlides = false;
+  $scope.active = 0;
+  var slides = $scope.slides = [];
+  var currIndex = 0;
+
+  $scope.addSlide = function() {
+    slides.push({
+      image: '/img/slide_' + currIndex + '.png',
+      id: currIndex++
+    });
+  };
+
+  for (var i = 0; i < 2; i++) {
+    $scope.addSlide();
+  }
+}
--- src/js/module.js
+++ src/js/module.js
@@ -1,1 +1,1 @@
-angular.module('RDash', ['ui.bootstrap', 'ui.router', 'ngCookies']);
+angular.module('RDash', ['ngAnimate', 'ngTouch', 'ui.bootstrap', 'ui.router', 'ngCookies']);
