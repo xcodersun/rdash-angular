b98a99ab755a1a90c5722a9375562cfa581d9226
asxcoder <xcodersun@gmail.com>
2017-01-01 15:18:40 -0800

    Add middle section in layout for title

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,7 +3,8 @@
 /**
  * Route configuration for the VivoDash module.
  */
-angular.module('VivoDash').config(['$stateProvider', '$urlRouterProvider',
+angular.module('VivoDash')
+.config(['$stateProvider', '$urlRouterProvider',
     function($stateProvider, $urlRouterProvider) {
 
         // For unmatched routes
@@ -23,6 +24,9 @@ angular.module('VivoDash').config(['$stateProvider', '$urlRouterProvider',
                     'layout-top@index': {
                         templateUrl: 'templates/layout/top.html',
                     },
+                    'layout-middle@index': {
+                        templateUrl: 'templates/layout/middle.html',
+                    },
                     'layout-main@index': {
                         templateUrl: 'templates/layout/main.html',
                     },
@@ -34,36 +38,65 @@ angular.module('VivoDash').config(['$stateProvider', '$urlRouterProvider',
                 templateUrl: 'templates/layout/sidebar.html',
             })
             .state('sidebar.home', {
-                url: '/home',
+                url: '/home/:title',
                 views: {
+                    'title@index' : {
+                        templateUrl: 'templates/layout/title.html',
+                        controller: 'TitleCtrl',
+                    },
                     'detail@index' : {
-                        templateUrl: 'templates/menu-home/home.html'
+                        templateUrl: 'templates/menu-home/home.html',
                     },
                 },
             })
             .state('sidebar.account_company_profile', {
-                url: '/account_company_profile',
+                url: '/account_company_profile/:title',
                 views: {
+                    'title@index' : {
+                        templateUrl: 'templates/layout/title.html',
+                        controller: 'TitleCtrl',
+                    },
                     'detail@index' : {
                         templateUrl: 'templates/menu-account/account_company_profile.html'
                     },
                 },
             })
             .state('sidebar.account_api_token', {
-                url: '/account_api_token',
+                url: '/account_api_token/:title',
                 views: {
+                    'title@index' : {
+                        templateUrl: 'templates/layout/title.html',
+                        controller: 'TitleCtrl',
+                    },
                     'detail@index' : {
                         templateUrl: 'templates/menu-account/account_api_token.html'
                     },
                 },
             })
             .state('sidebar.account_bill_plan', {
-                url: '/account_bill_plan',
+                url: '/account_bill_plan/:title',
                 views: {
+                    'title@index' : {
+                        templateUrl: 'templates/layout/title.html',
+                        controller: 'TitleCtrl',
+                    },
                     'detail@index' : {
                         templateUrl: 'templates/menu-account/account_bill_plan.html'
                     },
                 },
             })
     }
-]);
+]).controller('TitleCtrl', ['$scope', '$stateParams', TitleCtrl])
+
+function TitleCtrl($scope, $stateParams) {
+    title = $stateParams.title;
+    $scope.title = title.charAt(0).toUpperCase() + title.slice(1);
+    switch (title) {
+        case 'home':
+            $scope.icon = 'fa fa-home';
+            break;
+        case 'account':
+            $scope.icon = 'fa fa-user-circle-o';
+            break;
+    }
+}
