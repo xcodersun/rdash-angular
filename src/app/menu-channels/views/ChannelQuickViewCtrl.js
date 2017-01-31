fd00570db2c87d7f34ece7072576df2cc3b17dfb
asxcoder <xcodersun@gmail.com>
2017-01-30 23:26:10 -0800

    Implemented channel quick view charts
    - replace chart.js with nvd3.js
    - hook up eywa api for chart data

--- src/app/menu-channels/views/ChannelQuickViewCtrl.js
+++ src/app/menu-channels/views/ChannelQuickViewCtrl.js
@@ -4,7 +4,10 @@ angular.module('VivoDash')
 function ChannelQuickViewCtrl($scope, id, config, $cookies, $http) {
   var cqvc = this;
   var authToken = $cookies.getObject('authToken');
-  $scope.data = [];
+  $scope.charts = [];
+
+  var start = getStart();
+  var end = getEnd();
 
   $http({
     url: config.apiAdminChannels + '/' + id,
@@ -14,12 +17,10 @@ function ChannelQuickViewCtrl($scope, id, config, $cookies, $http) {
     },
   }).then(function (response) {
     cqvc.channel = response.data;
-    console.log(cqvc.channel);
     var fileds_data = [];
 
     angular.forEach(cqvc.channel["fields"], function(value, key) {
-      url = constructUrl(config.apiAdminQuerySeriesNoTags, cqvc.channel["id"], key, "avg");
-      console.log(url);
+      url = constructUrl(config.apiAdminQuerySeriesNoTags, cqvc.channel["id"], key, "avg", start, end);
 
       authToken = $cookies.getObject('authToken');
       $http({
@@ -29,74 +30,90 @@ function ChannelQuickViewCtrl($scope, id, config, $cookies, $http) {
           'Authentication': authToken.token
         },
       }).then(function (response) {
+        var chart = {}
         var data = {};
+        /* one chart can have multiple data so use dataset */
+        var dataset = [];
         data["key"] = key;
         data["values"] = [];
-        console.log(response.data);
         for (var i = 0; i < response.data.length; i++) {
           if (response.data[i]["value"]) {
             data["values"].push([response.data[i]["timestamp"], response.data[i]["value"]]);
           }
         }
-        console.log(data);
-
-        $scope.data.push(data);
-
+        dataset.push(data);
+        chart["data"] = dataset;
+        chart["option"] = genChartOption('lineChart', key);
+        $scope.charts.push(chart);
       }).catch(function (e) {
+        /* apiAdminQuerySeriesNotTags */
         console.log(e);
       });
     });
+
   }).catch(function (e) {
+    /* apiAdminChannels */
     console.log(e);
   });
-
-  $scope.options = {
-      chart: {
-          type: 'lineChart',
-          height: 450,
-          margin : {
-              top: 20,
-              right: 20,
-              bottom: 40,
-              left: 55
-          },
-          x: function(d){ return d[0]; },
-          y: function(d){ return d[1]; },
-          useInteractiveGuideline: true,
-          dispatch: {
-              stateChange: function(e){ console.log("stateChange"); },
-              changeState: function(e){ console.log("changeState"); },
-              tooltipShow: function(e){ console.log("tooltipShow"); },
-              tooltipHide: function(e){ console.log("tooltipHide"); }
-          },
-          xAxis: {
-              tickFormat: function(d) {
-                return d3.time.format('%m/%d/%y %H:%M')(new Date(d));
-              },
-          },
-          yAxis: {
-              axisLabel: 'Temperature',
-              tickFormat: function(d){
-                  return d3.format('.02f')(d);
-              },
-              axisLabelDistance: -10
-          },
-          yDomain: [25, 50]
-      },
-  };
 }
 
-function constructUrl(url, id, field, type) {
+function getStart() {
+  date = new Date();
+  date.setDate(date.getDate() -  7);
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  start = Math.floor(date.getTime());
+  return start;
+}
+
+function getEnd() {
+  date = new Date();
+  date.setDate(date.getDate());
+  date.setHours(23);
+  date.setMinutes(59);
+  date.setSeconds(59);
+  end = Math.floor(date.getTime());
+  return end;
+}
+
+function genChartOption(type, yAxislabel) {
+  chart = {
+    chart: {
+      type: type,
+      height: 200,
+      margin : {
+        top: 20,
+        right: 50,
+        bottom: 40,
+        left: 100
+      },
+      x: function(d){ return d[0]; },
+      y: function(d){ return d[1]; },
+      useInteractiveGuideline: true,
+      xAxis: {
+        tickFormat: function(d) {
+          return d3.time.format('%m/%d/%y %H:%M')(new Date(d));
+        },
+        rotateLabels: -15
+      },
+      yAxis: {
+        axisLabel: yAxislabel,
+        tickFormat: function(d){
+            return d3.format('.02f')(d);
+        },
+      },
+      xDomain: [start, end],
+    },
+  };
+  return chart
+}
+
+function constructUrl(url, id, field, type, start, end) {
   url = url.replace("%s", id);
   url = url.replace("%s", field);
   url = url.replace("%s", type);
 
-  date = new Date();
-  date.setDate(date.getDate() - date.getDay() + 1);
-  date.setHours(0);
-  date.setMinutes(0);
-  date.setSeconds(0);
-  start = Math.floor(date.getTime() / 1000);
   url = url.replace("%d", start.toString());
   url = url.replace("%d", "");
 
