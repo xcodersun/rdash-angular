58b89bfc49852b11d26ab421d46986790f022877
asxcoder <xcodersun@gmail.com>
2017-02-21 23:31:34 -0800

    Dynamic selection bar

--- src/app/menu-dashboards/widgets/DashboardWidgetTrendCtrl.js
+++ src/app/menu-dashboards/widgets/DashboardWidgetTrendCtrl.js
@@ -1,14 +1,59 @@
 angular.module('VivoDash')
-    .controller('DashboardWidgetTrendCtrl', ['$uibModalInstance', DashboardWidgetTrendCtrl]);
+	.controller('DashboardWidgetTrendCtrl', ['$scope', '$uibModalInstance', '$http', '$cookies', 'config', DashboardWidgetTrendCtrl]);
 
-function DashboardWidgetTrendCtrl($uibModalInstance) {
-  var dwtc = this;
+function DashboardWidgetTrendCtrl($scope, $uibModalInstance, $http, $cookies, config) {
+	var dwtc = this;
+	$scope.channels = [];
+	$scope.fields = [];
+	dwtc.channel = {};
 
-  dwtc.create = function () {
-    $uibModalInstance.close('create');
-  }
+	var authToken = $cookies.getObject('authToken');
+	$http({
+		url: config.apiAdminChannels,
+		method: 'GET',
+		headers: {
+			'Authentication': authToken.token
+		}
+	}).then(function (response) {
+		var channels = response.data;
+		for (var i = 0; i < channels.length; i++) {
+			var ch = {};
+			ch.index = i;
+			ch.name = channels[i].name;
+			ch.id = channels[i].id;
+			$scope.channels.push(ch);
+		}
+	}).catch(function (e) {
+		console.log(e);
+	})
 
-  dwtc.cancel = function() {
-    $uibModalInstance.close('cancel');
-  }
+	dwtc.create = function () {
+		$uibModalInstance.close('create');
+	}
+
+	dwtc.cancel = function() {
+		console.log(dwtc.channel);
+		console.log(dwtc.field);
+		$uibModalInstance.close('cancel');
+	}
+
+	dwtc.channelSelected = function() {
+		$http({
+			url: config.apiAdminChannels + '/' + dwtc.channel.id,
+			method: 'GET',
+			headers: {
+				'Authentication': authToken.token
+			},
+		}).then(function (response) {
+			$scope.fields = [];
+			var fields = response.data.fields;
+			var i = 0;
+			angular.forEach(fields, function(value, key) {
+				var field = {};
+				field.index = i++;
+				field.name = key;
+				$scope.fields.push(field);
+			});
+		})
+	}
 }
