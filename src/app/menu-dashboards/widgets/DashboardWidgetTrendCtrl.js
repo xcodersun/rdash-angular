f14a7ec35b8be827135c0c35469f800c062c13ea
asxcoder <xcodersun@gmail.com>
2017-02-22 23:35:08 -0800

    Dynamic time range and interval selection

--- src/app/menu-dashboards/widgets/DashboardWidgetTrendCtrl.js
+++ src/app/menu-dashboards/widgets/DashboardWidgetTrendCtrl.js
@@ -5,6 +5,19 @@ function DashboardWidgetTrendCtrl($scope, $uibModalInstance, $http, $cookies, co
 	var dwtc = this;
 	$scope.channels = [];
 	$scope.fields = [];
+	$scope.time_ranges = [{
+		index: 0,
+		label: "1 days",
+		value: 86400,
+	}, {
+		index: 1,
+		label: "3 days",
+		value: 259200,
+	}, {
+		index: 2,
+		label: "7 days",
+		value: 604800,
+	}];
 	dwtc.channel = {};
 
 	var authToken = $cookies.getObject('authToken');
@@ -32,14 +45,16 @@ function DashboardWidgetTrendCtrl($scope, $uibModalInstance, $http, $cookies, co
 	}
 
 	dwtc.cancel = function() {
-		console.log(dwtc.channel);
-		console.log(dwtc.field);
+		console.log($scope.channel);
+		console.log($scope.field);
+		console.log($scope.time_range);
+		console.log($scope.interval);
 		$uibModalInstance.close('cancel');
 	}
 
 	dwtc.channelSelected = function() {
 		$http({
-			url: config.apiAdminChannels + '/' + dwtc.channel.id,
+			url: config.apiAdminChannels + '/' + $scope.channel.id,
 			method: 'GET',
 			headers: {
 				'Authentication': authToken.token
@@ -55,5 +70,55 @@ function DashboardWidgetTrendCtrl($scope, $uibModalInstance, $http, $cookies, co
 				$scope.fields.push(field);
 			});
 		})
+	}
+
+	dwtc.timeRangeSelected = function() {
+		switch ($scope.time_range.index) {
+			case 0:
+				$scope.intervals = [{
+					index: 0,
+					label: "15 mins",
+					value: 900,
+				}, {
+					index: 1,
+					label: "30 mins",
+					value: 1800,
+				}, {
+					index: 2,
+					label: "60 mins",
+					value: 3600,
+				}];
+				break;
+			case 1:
+				$scope.intervals = [{
+					index: 0,
+					label: "30 mins",
+					value: 1800,
+				}, {
+					index: 1,
+					label: "60 mins",
+					value: 3600,
+				}, {
+					index: 2,
+					label: "90 mins",
+					value: 5400,
+				}];
+				break;
+			case 2:
+				$scope.intervals = [{
+					index: 0,
+					label: "90 mins",
+					value: 5400,
+				}, {
+					index: 1,
+					label: "3 hrs",
+					value: 10800,
+				}, {
+					index: 2,
+					label: "6 hrs",
+					value: 21600,
+				}];
+				break;
+		}
 	}
 }
