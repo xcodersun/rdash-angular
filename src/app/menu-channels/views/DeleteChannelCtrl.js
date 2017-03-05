643c3f4488ccf7883a844bddf1b264c73a8acb02
asxcoder <xcodersun@gmail.com>
2017-03-04 22:39:22 -0800

    Follow 2-space indentation rule

--- src/app/menu-channels/views/DeleteChannelCtrl.js
+++ src/app/menu-channels/views/DeleteChannelCtrl.js
@@ -1,5 +1,5 @@
 angular.module('VivoDash')
-    .controller('DeleteChannelCtrl', ['$uibModalInstance', 'channel', 'config', 'channelService', DeleteChannelCtrl]);
+  .controller('DeleteChannelCtrl', ['$uibModalInstance', 'channel', 'config', 'channelService', DeleteChannelCtrl]);
 
 function DeleteChannelCtrl($uibModalInstance, channel, config, channelService) {
   var dcc = this;
--- src/app/menu-channels/views/EditChannelWarningCtrl.js
+++ src/app/menu-channels/views/EditChannelWarningCtrl.js
@@ -1,10 +1,10 @@
 angular.module('VivoDash')
-    .controller('EditChannelWarningCtrl', ['$uibModalInstance', EditChannelWarningCtrl]);
+  .controller('EditChannelWarningCtrl', ['$uibModalInstance', EditChannelWarningCtrl]);
 
 function EditChannelWarningCtrl($uibModalInstance) {
-	var ecwc = this;
+  var ecwc = this;
 
-	ecwc.close = function() {
-		$uibModalInstance.close('');
-	}
+  ecwc.close = function() {
+    $uibModalInstance.close('');
+  }
 }
