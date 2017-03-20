5940f5fd389237638465e36e4e0a6eb958911d42
asxcoder <xcodersun@gmail.com>
2017-03-19 23:20:25 -0700

    dashboard edit button fills in existing info

--- src/app/menu-dashboards/DashboardsSummaryCtrl.js
+++ src/app/menu-dashboards/DashboardsSummaryCtrl.js
@@ -3,23 +3,27 @@ angular.module('VivoDash')
 
 function DashboardsSummaryCtrl($scope, $uibModal, $state, channelService, dashboardService, chartService, utilService) {
   var dsc = this;
-  $scope.charts = [];
+  $scope.dashboards = [];
 
   dashboardService.getAllDashboards()
   .then(function (dashboards) {
-    dsc.dashboards = dashboards;
     for (var i = 0; i < dashboards.length; i++) {
       // Traverse dashboards
       dashboardService.getDashboard(dashboards[i].id)
       .then(function (dashboard) {
+        var db = dashboard;
         var query = angular.fromJson(dashboard.definition);
         var start = utilService.getDateFromNow(-(query.time_range/86400), 'start');
         var end = utilService.getDateFromNow(0, 'end');
-        var url = channelService.getCustomSingleTrendUrl(query.id, query.field, 'avg', start, end, query.interval);
+        var url = channelService.getCustomSingleTrendUrl(query.channel_id, query.field, 'avg', start, end, query.interval);
         // Create chart
         chartService.singleTrend(url, query.field, start, end)
         .then(function (chart) {
-          $scope.charts.push(chart);
+          var item = {};
+          item.dashboard = db;
+          item.chart = {};
+          item.chart = chart;
+          $scope.dashboards.push(item);
         }).catch(function (e) {
           console.log(e);
         }); // Create chart done
@@ -41,6 +45,11 @@ function DashboardsSummaryCtrl($scope, $uibModal, $state, channelService, dashbo
       controllerAs: 'dwtc',
       backdrop: 'static',
       size: 'lg',
+      resolve: {
+        dashboard: function() {
+          return
+        }
+      }
     });
     modalInstance.result.then(function() {
       var res = modalInstance.result.$$state.value;
@@ -50,11 +59,25 @@ function DashboardsSummaryCtrl($scope, $uibModal, $state, channelService, dashbo
     });
   };
 
-  dsc.editDashboard = function(index) {
-    console.log(dsc.dashboards[index]);
+  dsc.editDashboard = function(dashboard) {
+    var modalInstance = $uibModal.open({
+      animation: true,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'templates/menu-dashboards/widgets/dashboard_widget_trend.html',
+      controller: 'DashboardWidgetTrendCtrl',
+      controllerAs: 'dwtc',
+      backdrop: 'static',
+      size: 'lg',
+      resolve: {
+        dashboard: function() {
+          return dashboard
+        }
+      }
+    });
   }
 
-  dsc.deleteDashboard = function(index) {
-    console.log(dsc.dashboards[index]);
+  dsc.deleteDashboard = function(db_id) {
+    console.log(dsc.dashboards[db_id]);
   }
 }
--- src/app/menu-dashboards/widgets/DashboardWidgetTrendCtrl.js
+++ src/app/menu-dashboards/widgets/DashboardWidgetTrendCtrl.js
@@ -1,23 +1,58 @@
 angular.module('VivoDash')
-  .controller('DashboardWidgetTrendCtrl', ['$scope', '$uibModalInstance', 'channelService', 'dashboardService', 'flashService', DashboardWidgetTrendCtrl]);
+  .controller('DashboardWidgetTrendCtrl', ['$scope', '$uibModalInstance', 'dashboard', 'channelService', 'dashboardService', 'flashService', DashboardWidgetTrendCtrl]);
 
-function DashboardWidgetTrendCtrl($scope, $uibModalInstance, channelService, dashboardService, flashService) {
+function DashboardWidgetTrendCtrl($scope, $uibModalInstance, dashboard, channelService, dashboardService, flashService) {
   var dwtc = this;
   $scope.channels = [];
   $scope.fields = [];
   $scope.time_ranges = [{
-    index: 0,
     label: "1 days",
     value: 86400,
+    intervals: [{
+      label: "15 mins",
+      value: 900,
+    }, {
+      label: "30 mins",
+      value: 1800,
+    }, {
+      label: "60 mins",
+      value: 3600,
+    }],
   }, {
-    index: 1,
     label: "3 days",
     value: 259200,
+    intervals: [{
+      label: "30 mins",
+      value: 1800,
+    }, {
+      label: "60 mins",
+      value: 3600,
+    }, {
+      label: "90 mins",
+      value: 5400,
+    }],
   }, {
-    index: 2,
     label: "7 days",
     value: 604800,
+    intervals: [{
+      label: "90 mins",
+      value: 5400,
+    }, {
+      label: "3 hrs",
+      value: 10800,
+    }, {
+      label: "6 hrs",
+      value: 21600,
+    }],
   }];
+
+  // For dashboard edition
+  var definition;
+  if (!angular.isUndefined(dashboard)) {
+    definition = angular.fromJson(dashboard.definition);
+    $scope.name = dashboard.name;
+    $scope.description = dashboard.description;
+  }
 
   channelService.getAllChannels()
   .then(function (channels) {
@@ -27,14 +62,65 @@ function DashboardWidgetTrendCtrl($scope, $uibModalInstance, channelService, das
       ch.name = channels[i].name;
       ch.id = channels[i].id;
       $scope.channels.push(ch);
+      // For dashboard edition
+      if (!angular.isUndefined(definition) && ch.id == definition.channel_id) {
+        $scope.channel = ch;
+        for (var i = 0; i < $scope.time_ranges.length; i++) {
+          if ($scope.time_ranges[i].value == definition.time_range) {
+            $scope.time_range = $scope.time_ranges[i];
+            for (var j = 0; j < $scope.time_ranges[i].intervals.length; j++) {
+              if ($scope.time_ranges[i].intervals[j].value == definition.interval) {
+                $scope.interval = $scope.time_ranges[i].intervals[j];
+                break;
+              }
+            }
+            break;
+          }
+        }
+        dwtc.channelSelected();
+        dwtc.timeRangeSelected();
+      }
     }
   }).catch(function (e) {
     console.log(e);
   });
 
+  dwtc.channelSelected = function() {
+    channelService.getChannel($scope.channel.id)
+    .then(function (channel) {
+      $scope.fields = [];
+      var fields = channel.fields;
+      var i = 0;
+      angular.forEach(fields, function(value, key) {
+        var field = {};
+        field.index = i++;
+        field.name = key;
+        $scope.fields.push(field);
+        if (!angular.isUndefined(definition) && field.name == definition.field) {
+          $scope.field = field;
+        }
+      });
+    })
+  }
+
+  dwtc.timeRangeSelected = function() {
+    switch ($scope.time_range.value) {
+      case 86400:
+        $scope.intervals = $scope.time_ranges[0].intervals;
+        break;
+      case 259200:
+        $scope.intervals = $scope.time_ranges[1].intervals;
+        break;
+      case 604800:
+        $scope.intervals = $scope.time_ranges[2].intervals;
+        break;
+    }
+  }
+
   dwtc.create = function () {
     dwtc.channel = {};
-    dwtc.channel.id = $scope.channel.id;
+    dwtc.channel.channel_id = $scope.channel.id;
+    dwtc.channel.channel_name = $scope.channel.name;
     dwtc.channel.field = $scope.field.name;
     dwtc.channel.time_range = $scope.time_range.value;
     dwtc.channel.interval = $scope.interval.value;
@@ -48,6 +134,7 @@ function DashboardWidgetTrendCtrl($scope, $uibModalInstance, channelService, das
     var data = angular.toJson(dwtc.dashboard);
     dashboardService.createDashboard(data)
     .then(function (response) {
+      console.log(response);
       $uibModalInstance.close('create');
     }).catch(function (e) {
       console.log(e);
@@ -57,70 +144,5 @@ function DashboardWidgetTrendCtrl($scope, $uibModalInstance, channelService, das
 
   dwtc.cancel = function() {
     $uibModalInstance.close('cancel');
-  }
-
-  dwtc.channelSelected = function() {
-    channelService.getChannel($scope.channel.id)
-    .then(function (channel) {
-      $scope.fields = [];
-      var fields = channel.fields;
-      var i = 0;
-      angular.forEach(fields, function(value, key) {
-        var field = {};
-        field.index = i++;
-        field.name = key;
-        $scope.fields.push(field);
-      });
-    })
-  }
-
-  dwtc.timeRangeSelected = function() {
-    switch ($scope.time_range.index) {
-      case 0:
-        $scope.intervals = [{
-          index: 0,
-          label: "15 mins",
-          value: 900,
-        }, {
-          index: 1,
-          label: "30 mins",
-          value: 1800,
-        }, {
-          index: 2,
-          label: "60 mins",
-          value: 3600,
-        }];
-        break;
-      case 1:
-        $scope.intervals = [{
-          index: 0,
-          label: "30 mins",
-          value: 1800,
-        }, {
-          index: 1,
-          label: "60 mins",
-          value: 3600,
-        }, {
-          index: 2,
-          label: "90 mins",
-          value: 5400,
-        }];
-        break;
-      case 2:
-        $scope.intervals = [{
-          index: 0,
-          label: "90 mins",
-          value: 5400,
-        }, {
-          index: 1,
-          label: "3 hrs",
-          value: 10800,
-        }, {
-          index: 2,
-          label: "6 hrs",
-          value: 21600,
-        }];
-        break;
-    }
   }
 }
